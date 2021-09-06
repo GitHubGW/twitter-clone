1fc022474d28cf170fc7c41a4d8fcf45ff204a7b
GW <kowonp@gmail.com>
2021-09-07 02:42:25 +0900

    Make Firebase Configuration

---
+++ src/firebaseConfiguration.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const defaultAnalytics = firebase.analytics(firebaseConfig);
