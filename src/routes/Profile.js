0715a140691f19382c3847b59ac0b222fb25ad2e
GW <kowonp@gmail.com>
2021-09-08 04:40:30 +0900

    Use useHistory hook

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,24 @@
+import { useHistory } from "react-router-dom";
+import { authService } from "firebaseConfiguration";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const history = useHistory();
+
+  const onClickLogOut = async () => {
+    const currentUser = authService.currentUser;
+
+    if (currentUser) {
+      await authService.signOut();
+      history.push("/");
+    }
+  };
+
+  return (
+    <>
+      <h1>Profile</h1>
+      <button onClick={onClickLogOut}>로그아웃</button>
+    </>
+  );
 };
 
 export default Profile;
