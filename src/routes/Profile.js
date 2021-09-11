863cc6c7fa615ea7bb0e5041d3794d8ca74e7121
GW <kowonp@gmail.com>
2021-09-12 03:01:58 +0900

    Get my own Tweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,12 @@
 import { useHistory } from "react-router-dom";
-import { authService } from "firebaseConfiguration";
+import { authService, firestoreService } from "firebaseConfiguration";
+import { useEffect } from "react";
 
-const Profile = () => {
+const Profile = ({ userObject }) => {
+  console.log("Profile userObject", userObject);
+
+  const FIRESTORE_COLLECTION = "tweets";
+
   const history = useHistory();
 
   const onClickLogOut = async () => {
@@ -12,6 +17,21 @@ const Profile = () => {
       history.push("/");
     }
   };
+
+  const getMyTweets = async () => {
+    const tweets = await firestoreService.collection(FIRESTORE_COLLECTION).where("uid", "==", userObject.uid).orderBy("createdAtTime", "desc").get();
+    // Document에서 특정한 필드의 데이터만 가져오기
+    // tweets.docs.map((doc)=>doc.get("content"))
+
+    // Document에서 모든 필드의 데이터 가져오기
+    // tweets.docs.map((doc)=>doc.data())
+
+    tweets.docs.map((doc) => console.log("getMyTweets doc", doc.data()));
+  };
+
+  useEffect(() => {
+    getMyTweets();
+  }, []);
 
   return (
     <>
