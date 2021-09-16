a7cc89d8bf86069842da4f6c76a55dd2942648c0
GW <kowonp@gmail.com>
2021-09-16 16:02:29 +0900

    Loading page

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
+const LoadingContainer = styled.section`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LoadingIcon = styled(FontAwesomeIcon)`
+  font-size: 40px;
+  color: #1da1f2;
+`;
+
+const LoadingTitle = styled.h1`
+  font-size: 16px;
+  color: #1da1f2;
+`;
+
 const Loading = () => {
-  return <h1>Loading...🧑🏻‍💻</h1>;
+  return (
+    <LoadingContainer>
+      <LoadingIcon icon={faTwitter}></LoadingIcon>
+      <LoadingTitle>Loading...</LoadingTitle>
+    </LoadingContainer>
+  );
 };
 
 export default Loading;
